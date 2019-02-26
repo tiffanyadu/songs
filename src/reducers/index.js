893676b0ae540941f70ec40c02b72054e4608d75
tiffanyadu <tadofdesign@gmail.com>
2019-02-25 19:11:01 -0600

    Create reducer to get song list

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+const songsReducer = () => {
+  return [
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "Macarena", duration: "2:30" },
+    { title: "All Star", duration: "3:15" },
+    { title: "I Want It That Way", duration: "1:45" }
+  ];
+};
