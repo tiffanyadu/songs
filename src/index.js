c184e845c9e94c4cca37cba772f6fe51c982a94a
tiffanyadu <tadofdesign@gmail.com>
2019-02-25 19:24:57 -0600

    Wire up Provider and create store

--- src/index.js
+++ src/index.js
@@ -6,4 +6,9 @@ import { createStore } from "redux";
 import App from "./components/App";
 import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
