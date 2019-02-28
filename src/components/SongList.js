94d636caa5cfb0e6df0aeb2015d76bb2017dafdf
tiffanyadu <tadofdesign@gmail.com>
2019-02-27 19:28:35 -0600

    Call selectSong action on click to return currently selected song

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -5,11 +5,15 @@ import { selectSong } from "../actions";
 class SongList extends Component {
   renderList() {
     return this.props.songs.map(song => {
-      console.log(song.title);
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">Select</button>
+            <button
+              onClick={() => this.props.selectSong(song)}
+              className="ui button primary"
+            >
+              Select
+            </button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -17,12 +21,12 @@ class SongList extends Component {
     });
   }
   render() {
-    console.log(this.props);
     return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
 const mapStateToProps = state => {
+  console.log(state);
   return { songs: state.songs };
 };
 
