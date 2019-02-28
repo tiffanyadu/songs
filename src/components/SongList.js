8d7b04d35798ff1e2af3170e3d150d3a8002da54
tiffanyadu <tadofdesign@gmail.com>
2019-02-27 19:19:02 -0600

    Import and add selectSong action to connect

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends Component {
   renderList() {
@@ -16,6 +17,7 @@ class SongList extends Component {
     });
   }
   render() {
+    console.log(this.props);
     return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
@@ -24,4 +26,7 @@ const mapStateToProps = state => {
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(
+  mapStateToProps,
+  { selectSong }
+)(SongList);
